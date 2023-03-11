09494907481e87a1da19a8a3f926428794117ffa
mlstanley <mlstanley@gmail.com>
2023-03-11 14:42:18 -0800

    auth -test

---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div class="flex flex-1 h-full justify-center justify-items-center align-middle">
+     <div class="card w-96 bg-secondary shadow-xl m-auto">
+         <div class="card-body items-center text-center">
+             <h2 class="card-title text-secondary-content">Login</h2>
+             <p><Icon class="text-4xl w-full h-28" name="logos:discord" /></p>
+             <div class="card-actions">
+                 <button class="btn-secondary btn-lg" @click="discordLogin">Login</button>
+             </div>
+         </div>
+     </div>
+ </div>
+ </template>
+ <script setup>
+ const { discordLogin } = useAuth()
+ 
+ 
+ 
+ 
+ </script>
