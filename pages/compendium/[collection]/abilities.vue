a931bf9dc5e846fbc77b8237aaf398673f1e54f5
mlstanley <mlstanley@gmail.com>
2024-01-29 17:18:19 -0800

    ablility kind update

--- pages/compendium/[collection]/abilities.vue
+++ pages/compendium/[collection]/abilities.vue
@@ -12,14 +12,43 @@
           >
             <div
               v-for="abilityKey in Object.keys(abilities)"
-              class="shadow-xl p-3 card card-compact w-full bg-base-100 h-96 min-w-98 sm:mb-2"
+              class="shadow-xl p-3 card card-compact w-full bg-base-100 min-h-96 min-w-98 sm:mb-2"
               :key="ability"
             >
-              <div class="card-body">
+              <div class="card-body h-full">
                 <h2 class="card-title">{{ abilities[abilityKey].name }}</h2>
-                <p class="line-clamp-5 max-h-24">
+                <div
+                  v-if="abilities[abilityKey].pool"
+                  class="badge badge-primary m-1 capitalize"
+                >
+                  {{ abilities[abilityKey].pool }}:
+                  {{ abilities[abilityKey].cost }}
+                </div>
+                <div
+                  v-if="abilities[abilityKey].tier"
+                  class="badge m-1 capitalize"
+                >
+                  tier: {{ abilities[abilityKey].tier }}
+                </div>
+                <div
+                  v-if="abilities[abilityKey].tier"
+                  class="badge m-1 capitalize"
+                >
+                  kind: {{ abilities[abilityKey].kind }}
+                </div>
+                <div
+                  v-if="abilities[abilityKey].categories"
+                  v-for="category in abilities[abilityKey].categories"
+                  class="badge badge-accent m-1 capitalize"
+                >
+                  {{ category.split('_').join(' ').trim() }}
+                </div>
+                <div class="divider"></div>
+                <p class="line-clamp-5 max-h-full">
                   {{ abilities[abilityKey].description }}
+                  {{ abilities[abilityKey].kind }}
                 </p>
+
                 <div class="card-actions justify-end mt-2">
                   <button
                     class="btn btn-primary"
