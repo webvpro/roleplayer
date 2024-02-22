47ccf8c73e14febb8520ab507a6558f163c5626c
mlstanley <mlstanley@gmail.com>
2024-02-22 11:46:18 -0800

    profile and main nav undates rollback

--- layouts/sheet.vue
+++ layouts/sheet.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="flex flex-col h-screen">
-    <NavPageHeaderMain />
+    <div
+      class="navbar sticky top-0 z-30 bg-primary text-primary-content max-h-12"
+    >
+      <div class="navbar-start">
+        <div class="dropdown sm:hidden">
+          <button class="btn btn-square btn-ghost text-base-content text-2xl">
+            <Icon name="ic:outline-menu" />
+          </button>
+          <ul
+            tabindex="0"
+            class="dropdown-content menu bg-base-100 text-base-content w-56"
+          >
+            <li><NuxtLink to="/compendium">Compendiums</NuxtLink></li>
+            <li><NuxtLink to="/characters">Characters</NuxtLink></li>
+            <li><NuxtLink to="/play">Play</NuxtLink></li>
+          </ul>
+        </div>
+        <a href="/" class="btn btn-ghost normal-case md:text-xl text-sm"
+          >PlayCypher.com</a
+        >
+      </div>
+      <div class="navbar-center">
+        <div class="hidden md:block">
+          <ul
+            class="menu menu-horizontal bg-base-100 text-base-content rounded-box"
+          >
+            <li><NuxtLink to="/compendium">Compendiums</NuxtLink></li>
+            <li><NuxtLink to="/characters">Characters</NuxtLink></li>
+            <li><NuxtLink to="/play">Play</NuxtLink></li>
+          </ul>
+        </div>
+      </div>
+      <div class="navbar-end">
+        <ProfileNav />
+      </div>
+    </div>
     <main id="main-body" class="relative flex flex-col flex-1 overflow-hidden">
       <div class="drawer drawer-end h-full mb-3 w-auto">
         <input
